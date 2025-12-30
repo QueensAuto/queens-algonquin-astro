9a45f015786daea56357ba58f88ac09905bc5762
QueensAuto <queensmobile11@gmail.com>
2025-12-30 15:36:13 -0600

    Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://queensautoservices.com',
-  integrations: [sitemap()],
+  integrations: [],
   devToolbar: {
     enabled: false
   },
