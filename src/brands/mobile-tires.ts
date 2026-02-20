5e183692d7763026716212057c9636a2ea849c53
QueensAuto <queensmobile11@gmail.com>
2026-02-20 15:26:45 -0600

    Update 02-20-26

--- src/brands/mobile-tires.ts
+++ src/brands/mobile-tires.ts
@@ -12,7 +12,7 @@ export const mobileTiresBrand: BrandConfig = {
     email: "service@queensautoservices.com",
     address: "2401 E Algonquin Rd, Algonquin, IL 60102",
     cityLabel: "Algonquin, IL",
-    googleMapsUrl: "https://www.google.com/maps/place/2401+E+Algonquin+Rd,+Algonquin,+IL+60102/@42.1491761,-88.2619717,17z/",
+    googleMapsUrl: "https://maps.app.goo.gl/Y569UdAXUnBz7rSW9",
     hours: {
         monFri: "7:30 - 6:00 pm",
         sat: "7:30 - 4:00 pm",
